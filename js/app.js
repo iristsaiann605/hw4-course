60d078c52765ebb44b408e416c6e47dc8dac373c
iristsaiann605 <iristsaiann605@gmail.com>
2014-11-21 21:31:13 +0800

    test8
    把picture大圖插入

--- js/app.js
+++ js/app.js
@@ -66,9 +66,11 @@ var loadPagesInfo = function(pages){
       $page.find('.title a').text(response.name).attr('href',response.link);
       $page.find('.about').text(response.about);
       $page.find('.likes').text(response.likes);
-      FB.api(/*輸入圖片連結*/, function(response){
+      FB.api(item.id+'/picture?type=large', function(response){
         // 塞資料到 html 中
+        $page.find('thumbnail img').attr('src',response.data.url);
         counter++;
+        $page.appendTo(current);
         // 塞完資料以後處理一下斷行
         if(counter===pages.length){
           // 利用 .current div:nth-child(3n)，讓每三個page 斷行
