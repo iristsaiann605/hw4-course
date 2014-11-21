66ad8bb6890d5f6cb6d9f9778fe61834440a357b
iristsaiann605 <iristsaiann605@gmail.com>
2014-11-21 21:03:55 +0800

    test3

--- js/app.js
+++ js/app.js
@@ -26,7 +26,7 @@ window.fbAsyncInit = function() {
             FB.api( '/me?fields=name,picture', function(response){
               // 把資訊插入到html裡，並顯示出來
               $('.user-name').text(response.name);
-              $('.user-photo').attr('scr', response.picture.data.url);
+              $('.user-photo').attr('src', response.picture.data.url);
               $('#user').removeClass('hide');
               // ---------------
               // 讀取 like 的列表，並儲存到 likes, 以及下一組資料的連結到 next
